ce1af11f441e62cde4cd3e4e8aafc860744ff78d
Krishna Alex <krishna.aromal@gmail.com>
2023-08-15 14:54:16 -0700

    Initial Commit

---
+++ NagMeReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NagMeReminder
+//
+//  Created by Krishna Alex on 8/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
